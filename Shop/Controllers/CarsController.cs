27950e6c68020894f240eb9fa9ce8f3348ae5c3e
Влад Капцан <vladkaptsan32@gmail.com>
2021-01-21 02:44:12 +0300

    обнова

---
+++ Shop/Controllers/CarsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller 
+    {
+        private readonly IAllCars _allCars;
+        private readonly ICarsCategory _allCategories;
+    }
+}
--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data.Interfaces;
+using Shop.Data.mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,8 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IAllCars, MockCars>();
+            services.AddTransient<ICarsCategory, MockCategory>();
             services.AddMvc();
         }
 
