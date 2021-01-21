0808773fbd2f149ff04b482e0863d213777e57c6
Влад Капцан <vladkaptsan32@gmail.com>
2021-01-21 15:24:14 +0300

    1

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -29,7 +29,7 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc(option => option.EnableEndpointRouting = false);
-            services.AddDbContext<AppDBContent>(options => options.UseMySQL(_confSting.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confSting.GetConnectionString("DefaultConnection")));
             services.AddTransient<IAllCars, CarRepository>();
             services.AddTransient<ICarsCategory, CategoryRepository>();
             services.AddMvc();
