85d04533f9133dd1e50f64279b33abd675e45ce5
Влад Капцан <vladkaptsan32@gmail.com>
2021-01-21 15:14:12 +0300

    1

--- Shop/Data/Repository/CategoryRepository.cs
+++ Shop/Data/Repository/CategoryRepository.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shop.Data.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICarsCategory
     {
+
+        private readonly AppDBContent appDBContent;
+
+        public CategoryRepository(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+
+        public IEnumerable<Category> AllCategories => appDBContent.Category;
     }
 }
