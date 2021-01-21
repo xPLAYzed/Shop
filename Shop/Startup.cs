5b2cd6ca6e0a47b29a0e1abd000c6a1fceb12c6d
Влад Капцан <vladkaptsan32@gmail.com>
2021-01-21 05:23:41 +0300

    Влад3

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -18,6 +18,7 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+          
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddTransient<IAllCars, MockCars>();
             services.AddTransient<ICarsCategory, MockCategory>();
