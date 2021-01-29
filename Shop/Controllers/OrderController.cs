87d08effb4f9b7073e35822434c6b3c41512fa59
Влад Капцан <vladkaptsan32@gmail.com>
2021-01-29 23:42:16 +0300

    1

---
+++ Shop/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class OrderController : Controller 
+    {
+
+        private readonly IAllOrders allOrders ;
+        private readonly ShopCart shopCart;
+
+
+        public OrderController(IAllOrders allOrders, ShopCart shopCart)
+        {
+            this.allOrders = allOrders;
+            this.shopCart = shopCart;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+
+    }
+}
